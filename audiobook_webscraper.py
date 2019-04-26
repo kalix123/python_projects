25a1bcc80416d041b1dba936760c4d4815373b5f
Kalix Zee <zeekalix@gmai.com>
2019-04-26 08:20:17 -0700

    removed pyqt5 since it was obselete. works if you want all 37 links at once. testin with another book now

--- audiobook_webscraper.py
+++ audiobook_webscraper.py
@@ -1,32 +1,15 @@
 import sys
 import re
-import urllib.request
+import requests
 from bs4 import BeautifulSoup
-from PyQt5.QtWebEngineWidgets import QWebEnginePage
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QUrl
+
 
 chapter = int(input("CHAPTER: ")) - 1
 
-class Page(QWebEnginePage):
-    def __init__(self, url):
-        self.app = QApplication(sys.argv)
-        QWebEnginePage.__init__(self)
-        self.html = ''
-        self.loadFinished.connect(self._on_load_finished)
-        self.load(QUrl(url))
-        self.app.exec_()
 
-    def _on_load_finished(self):
-        self.html = self.toHtml(self.Callable)
-        # print('Load finished')
-
-    def Callable(self, html_str):
-        self.html = html_str
-        self.app.quit()
-
-page = Page('https://azkabanaudiobook.com/deathly-hallows-jim-dale-book-7/#more-173')
-soup = BeautifulSoup(page.html, 'html.parser')
+page = requests.get('https://azkabanaudiobook.com/deathly-hallows-jim-dale-book-7/#more-173')
+html = page.content
+soup = BeautifulSoup(html, 'html.parser')
 data = []
 links = []
 regex = r"openload\.co/+.*?mime=true"
@@ -38,19 +21,23 @@ for frame in soup.findAll("iframe"):
 for x in range(len(data)):
     if not x % 2:
         links.append(data[x][2])
-page = Page(links[chapter])
-soup = BeautifulSoup(page.html, 'html.parser')
-for y in soup.findAll('script'):
-    for script in y:
-        if "$(\"#jquery_jplayer_1\").jPlayer({" in script:
-            audio.append(script)
 
-for code in audio:
-    download_links.append(re.findall(regex,code))
+for link in links:
+    page = requests.get(link)
+    html = page.content
+    soup = BeautifulSoup(html, 'html.parser')
+    for y in soup.findAll('script'):
+        for script in y:
+            if "$(\"#jquery_jplayer_1\").jPlayer({" in script:
+                audio.append(script)
+                for code in audio:
+                    download_links.append(re.findall(regex,code))
+
+
 
 
 
 # # print(re.findall(regex,audio[0]))
 # for link in links:
 #     print(link)
-print(download_links[0][0])
+print(download_links)
